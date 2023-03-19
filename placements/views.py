0da6d986e351a4793f198fbfe46682d7eb9e25d5
anuragdevon <msm19b021@iiitdm.ac.in>
2023-03-19 20:25:07 +0530

    add views for serialiers queryset

--- placements/views.py
+++ placements/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Student, Company, JobPosting, Placement
+from .serializers import StudentSerializer, CompanySerializer, JobPostingSerializer, PlacementSerializer
 
-# Create your views here.
+class StudentList(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class CompanyList(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class CompanyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class JobPostingList(generics.ListCreateAPIView):
+    queryset = JobPosting.objects.all()
+    serializer_class = JobPostingSerializer
+
+class JobPostingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = JobPosting.objects.all()
+    serializer_class = JobPostingSerializer
+
+class PlacementList(generics.ListCreateAPIView):
+    queryset = Placement.objects.all()
+    serializer_class = PlacementSerializer
+
+class PlacementDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Placement.objects.all()
+    serializer_class = PlacementSerializer
