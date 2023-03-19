b6081e0909ac9d5ee15b2d2c8b47e27f24985b10
anuragdevon <msm19b021@iiitdm.ac.in>
2023-03-19 20:14:14 +0530

    create model serializers

---
+++ placements/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Student, Company, JobPosting, Placement
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+class JobPostingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobPosting
+        fields = '__all__'
+
+class PlacementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Placement
+        fields = '__all__'
