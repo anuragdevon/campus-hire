cbf3bc5af04f78f7eacce17425876f1f4cd11d9d
anuragdevon <msm19b021@iiitdm.ac.in>
2023-03-19 20:44:12 +0530

    set url patterns

--- placements/urls.py
+++ placements/urls.py
@@ -1,6 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import StudentList, StudentDetail, CompanyList, CompanyDetail, JobPostingList, JobPostingDetail, PlacementList, PlacementDetail
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
+    path('students/', StudentList.as_view()),
+    path('students/<int:pk>/', StudentDetail.as_view()),
+    path('companies/', CompanyList.as_view()),
+    path('companies/<int:pk>/', CompanyDetail.as_view()),
+    path('jobpostings/', JobPostingList.as_view()),
+    path('jobpostings/<int:pk>/', JobPostingDetail.as_view()),
+    path('placements/', PlacementList.as_view()),
+    path('placements/int:pk/', PlacementDetail.as_view()),
 ]
