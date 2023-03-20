4caa27c30ec6cfc9b14cc07326c30895a095167b
anuragdevon <msm19b021@iiitdm.ac.in>
2023-03-20 12:05:18 +0530

    add admin setup

--- placements/admin.py
+++ placements/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import JobPosting, Company, Student
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'roll_number')
+    list_filter = ('status',)
+
+@admin.register(Company)
+class CompanyProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'website', 'email')
+    list_filter = ('name',)
+
+@admin.register(JobPosting)
+class JobOpportunityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'status')
+    list_filter = ('company','title', 'status')
+
+
